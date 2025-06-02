b61e2f81e641344d55b00c7da00fbcbfdba6d64c
Julya-Tkachenko <julyagroma@gmail.com>
2025-06-02 11:51:49 +0300

    add redux to the project

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 
 import './style.scss';
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 
 const rootNodeElement = document.querySelector("#main");
 const root = ReactDOM.createRoot(rootNodeElement);
-root.render(<div>Hello world</div>);
+root.render(<Provider store={store}>
+   <App/>
+</Provider>);
